bbf503606238e258b6acb035bca8d52aff3ab635
jaeseong <cpontina@naver.com>
2018-07-29 15:45:02 +0900

    reducer

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Timer from './components/Timer'
+import Timer from './components/Timer';
+import reducer from './reducer';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+//컴포넌트 안의 스토어를 복사해서 자식 컴포넌트에게 전송.
+
+let store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
-    return <Timer/>;
+    console.log(store.getState());
+    return (
+      <Provider store={store}>
+        <Timer/>
+      </Provider>
+    )
   }
 }
---
+++ reducer.js
@@ -0,0 +1,85 @@
+//Import
+
+//Action
+    const TIMER_START = 'TIMER_START';
+    const TIMER_RESTART = 'TIMER_RESTART';
+    const ADD_SECOND = 'ADD_SECOND';
+
+//Action Creators
+    function timerStart(){
+        return{
+            type:TIMER_START
+        }
+    }
+    function timerRestart(){
+        return{
+            type:TIMER_RESTART
+        }
+    }
+    function addSecond(){
+        return{
+            type:ADD_SECOND
+        }
+    }
+
+//Reducer
+
+    const TIMER_DURATION = 3600;
+
+    const initialState ={
+        isPlaying : false,
+        elapsedTime : 0,
+        maxTime : TIMER_DURATION
+    };
+
+    function reducer(state = initialState, action){
+        switch(action.type){
+            case TIMER_START:
+                return applyStartTimer(state);
+            case TIMER_RESTART:
+                return applyRestartTimer(state);
+            case ADD_SECOND:
+                return applyAddSecond(state);
+            default:
+                return state;
+        }
+    }
+
+//Reducer Function
+
+    function applyStartTimer(state){
+        return{
+            ...state,
+            isPlaying:true
+        }
+    }
+    function applyRestartTimer(state){
+        return{
+            ...state,
+            isPlaying:false,
+            elapsedTime:0
+        }
+    }
+    function applyAddSecond(state){
+        if(state.elapsedTime < TIMER_DURATION){
+            return{
+                ...state,
+                elapsedTime : elapsedTime+1
+            }
+        }else{
+            return{
+                ...state,
+                isPlaying:false
+            }
+        }
+    }
+
+//Export Action Creators
+    const actionCreators ={
+        timerStart,
+        timerRestart,
+        addSecond
+    };
+
+//Export Reducer
+    export default reducer;
